549ff8b7115e50f26eeecd2a0fc7edd0f8425256
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2025-02-13 12:02:54 +0200

    apply suggestions

--- src/auxiliary.jl
+++ src/auxiliary.jl
@@ -1,12 +1,12 @@
-threads_enabled = false
+const threads_enabled = Ref(false)
 
-enable_multithreading(enabled=true) = global threads_enabled = enabled
+enable_multithreading(enabled=true) = global threads_enabled[] = enabled
 disable_multithreading() = enable_multithreading(false)
 
 macro threaded(expr)
     # esc(quote ... end) as suggested in https://github.com/JuliaLang/julia/issues/23221
     return esc(quote
-        if Threads.nthreads() == 1 || !threads_enabled
+        if Threads.nthreads() == 1 || !threads_enabled[]
             $(expr)
         else
             Threads.@threads $(expr)
