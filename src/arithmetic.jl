67ae11a967f7ff385987afabd0a35dfa55c33527
Tom Finke <tom.finke@uni-a.de>
2026-09-14 16:44:09 +0200

    feat: implemented eoshift, running_sums and total_sums including unit tests

--- src/arithmetic.jl
+++ src/arithmetic.jl
@@ -48,3 +48,68 @@ function Base.circshift(sa::SecureArray, shifts)
 
     rotate(sa, shifts)
 end
+
+Base.:>>>(sa::SecureArray, shift::Integer) = circshift(sa, shift)
+
+# end-off shift
+"""
+    eoshift(sa::SecureArray, shifts)
+
+End-off shift, i.e., i.e. add `shifts` many zeros to the left of the data in `sa` and remove `shifts` elements from the right.
+
+Note: To precompute all required rotation indexes, use `init_rotation!`.
+
+See also: [`SecureArray`](@ref), [`init_rotation!`](@ref), [`circshift`](@ref), [Fortran EOSHIFT](https://gcc.gnu.org/onlinedocs/gfortran/EOSHIFT.html)
+"""
+function eoshift(sa::SecureArray, shifts)
+    mask = zeros(length(sa))
+    if shifts >= 0
+        mask[1+shifts:end] .= 1
+    else
+        mask[1:end+shifts] .= 1
+    end
+    return circshift(sa, (shifts,)) * PlainArray(mask, sa.context)
+end
+
+Base.:>>(sa::SecureArray, shift::Integer) = eoshift(sa, shift)
+
+"""
+    running_sums(v::SecureArray)
+
+Computes the running sums r such that ``r[i] = \\sum_{k=0}^i v[k] \\tex{for} \\i in [n]``.
+
+See also: [Halevi, S., & Shoup, V. (2014). Algorithms in HElib](https://eprint.iacr.org/2014/106)
+"""
+
+function running_sums(v::SecureArray{<:OpenFHEBackend})
+    n = length(v)
+    w = v
+    e = 1
+    while e < n
+        w = w + (w >> e)
+        e = 2 * e
+    end
+    return w
+end
+
+"""
+    total_sums(v::SecureArray)
+
+Computes the total sums t such that ``t[i] = \\sum_{k=0}^{n-1} v[k] \\tex{for} \\i in [n]``.
+
+See also: [Halevi, S., & Shoup, V. (2014). Algorithms in HElib](https://eprint.iacr.org/2014/106)
+"""
+function total_sums(v::SecureArray{<:OpenFHEBackend})
+    n = length(v)
+    w = v
+    e = 1
+    for j in (ndigits(n, base=2) - 2):-1:0
+        w = w + (w >>> e)
+        e = 2 * e
+        if n >> j & 1 == 1
+            w = v + (w >>> 1)
+            e = e + 1
+        end
+    end
+    return w
+end
